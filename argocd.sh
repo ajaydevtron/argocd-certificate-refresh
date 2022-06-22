5588ec8fc272eb3b5f14c3cc3dead370d022d08f
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2022-06-22 11:33:40 +0530

    Update argocd.sh

--- argocd.sh
+++ argocd.sh
@@ -5,5 +5,5 @@ sed -i '6,7d' argocd-secret.yaml
 kubectl delete secret argocd-secret -n devtroncd --ignore-not-found
 kubectl apply -f argocd-secret.yaml
 kubectl delete pods -n devtroncd -l app.kubernetes.io/name=argocd-server
-sleep 30
+sleep 15
 kubectl delete pods -n devtroncd -l component=devtron
