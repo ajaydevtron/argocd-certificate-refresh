23c53b08479abb34352ea42a293951ff5e7f92c2
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2022-09-08 14:03:46 +0530

    Update the argocd script

--- argocd.sh
+++ argocd.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 kubectl get secret argocd-secret -n devtroncd -o yaml > argocd-secret.yaml
-crtline=$(sed -n '/tls.crt/=' argocd-secret.yaml)
+crtline=$(sed -n '/tls.crt:/=' argocd-secret.yaml)
 sed -i "$crtline"d argocd-secret.yaml
-keyline=$(sed -n '/tls.key/=' argocd-secret.yaml)
+keyline=$(sed -n '/tls.key:/=' argocd-secret.yaml)
 sed -i "$keyline"d argocd-secret.yaml
 kubectl delete secret argocd-secret -n devtroncd --ignore-not-found
 kubectl apply -f argocd-secret.yaml
