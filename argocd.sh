e9daa69325553003b0e01ea6c79141430a139db4
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2022-09-08 12:47:10 +0530

    Update the argocd.sh

--- argocd.sh
+++ argocd.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 kubectl get secret argocd-secret -n devtroncd -o yaml > argocd-secret.yaml
-sed -i '6,7d' argocd-secret.yaml
+crtline=$(sed -n '/tls.crt/=' argocd-secret.yaml)
+sed -i "$crtline"d argocd-secret.yaml
+keyline=$(sed -n '/tls.key/=' argocd-secret.yaml)
+sed -i "$keyline"d argocd-secret.yaml
 kubectl delete secret argocd-secret -n devtroncd --ignore-not-found
 kubectl apply -f argocd-secret.yaml
 kubectl delete pods -n devtroncd -l app.kubernetes.io/name=argocd-server
