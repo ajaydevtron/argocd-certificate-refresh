20a7f5246774ec97a099783230f9b4b3a0441807
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2022-09-10 14:09:37 +0530

    Modified the script

--- argocd.sh
+++ argocd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 kubectl get secret argocd-secret -n devtroncd -o yaml > argocd-secret.yaml
-crtline=$(sed -n '/tls.crt:/=' argocd-secret.yaml)
+crtline=$(sed -n '/\btls.crt:.*\b/=' argocd-secret.yaml)
 sed -i "$crtline"d argocd-secret.yaml
-keyline=$(sed -n '/tls.key:/=' argocd-secret.yaml)
+keyline=$(sed -n '/\btls.key:.*\b/=' argocd-secret.yaml)
 sed -i "$keyline"d argocd-secret.yaml
-kubectl delete secret argocd-secret -n devtroncd --ignore-not-found
+cat argocd-secret.yaml
 kubectl apply -f argocd-secret.yaml
 kubectl delete pods -n devtroncd -l app.kubernetes.io/name=argocd-server
 sleep 15
