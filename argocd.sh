60404e0367001af341b1fe9be0051f8df3880e94
AJAY KUMAR YADAV <64467997+ajay0988@users.noreply.github.com>
2022-10-31 20:18:27 +0530

    added component level

--- argocd.sh
+++ argocd.sh
@@ -4,5 +4,5 @@ kubectl get secret argocd-secret -n devtroncd -o jsonpath="{.data.tls\.crt}" | b
 kubectl patch secret argocd-secret -n devtroncd --type=json -p='[{"op": "remove", "path": "/data/tls.crt"},{"op": "remove", "path": "/data/tls.key"}]'
 kubectl delete pods -n devtroncd -l app.kubernetes.io/name=argocd-server
 sleep 15
-kubectl delete pods -n devtroncd -l app=devtron
+kubectl delete pods -n devtroncd -l component=devtron
 kubectl get secret argocd-secret -n devtroncd -o jsonpath="{.data.tls\.crt}" | base64 -d | openssl x509 -enddate -noout
