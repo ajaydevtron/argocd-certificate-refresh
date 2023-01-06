ecc5cb7a6b5ed0c4ca8288123845499f05e6525f
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2023-01-06 12:36:29 +0530

    added the sleep time

--- argocd.sh
+++ argocd.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
-kubectl get secret argocd-secret -n devtroncd -o yaml > argocd-secret.yaml
-crtline=$(sed -n '/\btls.crt:.*\b/=' argocd-secret.yaml)
-sed -i "$crtline"d argocd-secret.yaml
-keyline=$(sed -n '/\btls.key:.*\b/=' argocd-secret.yaml)
-sed -i "$keyline"d argocd-secret.yaml
-cat argocd-secret.yaml
-kubectl apply -f argocd-secret.yaml
+kubectl get secret argocd-secret -n devtroncd -o jsonpath="{.data.tls\.crt}" | base64 -d | openssl x509 -enddate -noout
+kubectl patch secret argocd-secret -n devtroncd --type=json -p='[{"op": "remove", "path": "/data/tls.crt"},{"op": "remove", "path": "/data/tls.key"}]'
+sleep 4
 kubectl delete pods -n devtroncd -l app.kubernetes.io/name=argocd-server
 sleep 15
 kubectl delete pods -n devtroncd -l component=devtron
+kubectl get secret argocd-secret -n devtroncd -o jsonpath="{.data.tls\.crt}" | base64 -d | openssl x509 -enddate -noout
