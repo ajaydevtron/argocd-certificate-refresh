f61add04cbef5f691b632cd16f8dfe8a49530e8f
AJAY KUMAR <99399155+ajaydevtron@users.noreply.github.com>
2023-01-06 12:30:16 +0530

    Added the sleep time after patch

--- argocd.sh
+++ argocd.sh
@@ -2,6 +2,7 @@
 
 kubectl get secret argocd-secret -n devtroncd -o jsonpath="{.data.tls\.crt}" | base64 -d | openssl x509 -enddate -noout
 kubectl patch secret argocd-secret -n devtroncd --type=json -p='[{"op": "remove", "path": "/data/tls.crt"},{"op": "remove", "path": "/data/tls.key"}]'
+sleep 4
 kubectl delete pods -n devtroncd -l app.kubernetes.io/name=argocd-server
 sleep 15
 kubectl delete pods -n devtroncd -l component=devtron
